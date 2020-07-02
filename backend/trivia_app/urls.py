fdc354d8427aa277c0f5e71c1e0a4573c3b3cb97
Dori Runyon <dorirunyon@Doris-MacBook-Pro.local>
2020-07-02 11:50:30 -0700

    Basic setup

---
+++ backend/trivia_app/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import include, url  # noqa
+from django.urls import path
+from django.contrib import admin
+from django.shortcuts import redirect
+
+import django_js_reverse.views
+
+
+urlpatterns = [
+    path("", lambda request : redirect("/trivia_app/")),
+    path("admin/", admin.site.urls, name="admin"),
+    path("jsreverse/", django_js_reverse.views.urls_js, name="js_reverse"),
+    path("trivia_app/", include("trivia_app.urls"), name="trivia_app"),
+]
